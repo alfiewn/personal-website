282b55e6d257bcb2b860e4c16e77a657eeaf7ae9
alfiewn <alfiewn@gmail.com>
2021-05-15 12:27:18 +0100

    Add alt fields to images

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -7,13 +7,13 @@ import Row from 'react-bootstrap/Row'
 function SocialButtons(props) {
     return <Row className='mt-3 justify-content-center'>
         <a href='https://github.com/alfiewn'>
-            <img className='pr-2' src={githubIcon} />
+            <img className='pr-2' src={githubIcon} alt='Github Link'/>
         </a>
         <a href='https://www.linkedin.com/in/alfie-newman-20598b163/'>
-            <img src={linkedinIcon} />
+            <img src={linkedinIcon} alt='LinkedIn Link'/>
         </a>
         <a href='https://twitter.com/alfiewnewman'>
-            <img className='pl-2' src={twitterIcon} />
+            <img className='pl-2' src={twitterIcon} alt='Twitter Link'/>
         </a>
     </Row>
 }
