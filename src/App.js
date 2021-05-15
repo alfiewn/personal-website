311cff9d7976e7aea156be098c7cb338b00a8d0c
alfiewn <alfiewn@gmail.com>
2021-05-15 11:54:54 +0100

    Create landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import portrait from './portrait.png';
+import linkedinIcon from './linkedin.svg'
+import githubIcon from './github.svg'
+import twitterIcon from './twitter.svg'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+import Typist from 'react-typist';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Personal website for Alfie Newman.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/alfiewn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Find Me On GitHub
-        </a>
+        <Container className='mt-5'>
+
+          <Row className='justify-content-center'>
+            <img src={portrait} className="App-logo" alt="logo" />
+          </Row>
+          <Row className='mt-3 justify-content-center'>
+            <Typist>
+              Hi, I'm Alfie Newman. Welcome to my personal website!
+            </Typist>
+          </Row>
+          <Row className='mt-3 justify-content-center'>
+            <a href='https://github.com/alfiewn'>
+              <img className='pr-2' src={githubIcon} />
+            </a>
+            <a href='https://www.linkedin.com/in/alfie-newman-20598b163/'>
+              <img src={linkedinIcon} />
+            </a>
+            <a href='https://twitter.com/alfiewnewman'>
+              <img className='pl-2' src={twitterIcon} />
+            </a>
+          </Row>
+        </Container>
       </header>
     </div>
   );
