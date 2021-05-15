4029266214f9ff902a7a4206f6cea67771df6159
alfiewn <alfiewn@gmail.com>
2021-05-15 11:58:15 +0100

    Remove unneeded padding

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Container className='mt-5'>
+        <Container>
 
           <Row className='justify-content-center'>
             <img src={portrait} className="App-logo" alt="logo" />
